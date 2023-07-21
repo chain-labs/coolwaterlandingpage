684c8fe65e4f25f888026c568e3d17f142636f14
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2023-07-21 06:31:23 +0530

    Update BannerComp.tsx

--- src/components/Responsive/components/BannerComp.tsx
+++ src/components/Responsive/components/BannerComp.tsx
@@ -25,7 +25,7 @@ const BannerComp = () => {
 					src={BannerImage}
 					alt="BannerImage"
 				/>
-				{/* <div className="w-36 h-12 left-[150px] top-[178px] absolute">
+				<div className="w-36 h-12 left-[150px] top-[178px] absolute">
 					<div className="w-28 h-7 px-2.5 py-0.5 left-[22px] top-[22px] absolute bg-violet-50 rounded-3xl shadow-inner border border-zinc-300 justify-center items-center gap-1 inline-flex">
 						<div className="text-blue-600 text-xs font-medium leading-normal">
 							Find us on
@@ -33,18 +33,7 @@ const BannerComp = () => {
 						<div className="w-4 h-4 relative"></div>
 						<div className="w-4 h-4 relative"></div>
 					</div>
-				</div> */}
-				<button className="px-2.5 py-2 bg-slate-100 rounded-3xl shadow border border-white border-opacity-10 justify-center items-center flex">
-					<div className="text-white text-xs font-medium leading-normal">
-						Find us on
-					</div>
-					<div className="w-4 h-4 relative">
-						<Image
-							src={TwitterLogo}
-							alt="twitter"
-						/>
-					</div>
-				</button>
+				</div>
 				<div className="w-11 h-11 left-[41.35px] top-[195.54px] absolute">
 					<Image
 						src={Emoji}
